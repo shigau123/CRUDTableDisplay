8e5f8230225dd2fc475047dfd24bd3d42f400e29
sasi <sasikumar.9799@gmail.com>
2022-02-07 11:09:40 +0530

    entire priject has been added

---
+++ CRUDTableDisplay/CRUDoperationJSON/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import{HttpClientModule} from '@angular/common/http'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddEmployeeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,ReactiveFormsModule,HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
